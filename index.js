abd6c63f55d238915e3ab819185a683504a4507b
Mtduffey36 <michael.t.duffey@gmail.com>
2024-06-09 11:05:13 -0500

    added questions and packages required

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+      type: "input",
+      message: "What is the name of your application",
+      name: "appName",
+    },
+    {
+      type: "input",
+      message: "What does your application do?",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "How does a user install your app?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Can you explain how to use the app?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Anything in the app you'd like to give credit for?",
+      name: "credits",
+    },
+    {
+      type: "confirm",
+      message: "Would you like to include an MIT license?",
+      name: "license",
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
